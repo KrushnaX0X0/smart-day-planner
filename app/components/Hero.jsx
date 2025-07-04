43acb2c9e1864615ed16e67de24b90a2e80f88d1
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-07-04 21:50:47 +0530

    change ui

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,13 +5,13 @@ function Hero() {
   return (
     <div className='h-screen w-screen bg-black flex justify-center items-center flex-col relative'>
       <div className='flex justify-center items-center flex-col'>
-        <div className=' z-10 '>
-          <h1 className='text-6xl text-amber-50 inline-block font-semibold'>
-            <span className='text-8xl font-bold text-amber-300 font2 '> Plan </span> 
-            Your Day <br /> 
-             <span className=' ml-36'> Smarter with <span className='text-8xl font-bold text-amber-300 font2 '>AI</span>
+        <div className=' z-10 text-center '>
+          <h1 className='text-6xl text-amber-50 inline-block font-semibold sm:'>
+            <span className='text-8xl font-bold text-amber-300 font2  '> Plan </span>
+            Your Day <br />
+            <span className=' ml-36'> Smarter with <span className='text-8xl font-bold text-amber-300 font2 '>AI</span>
               <img src={brain.src} alt="hiii" className='h-20 inline-block up-down-animation ' />
-              </span></h1>
+            </span></h1>
         </div>
         <p className="mt-6 text-lg text-gray-400 tracking-[2px] md:text-xl z-11"> Your personal assistant for daily productivity, goals, and smart scheduling.</p>
       </div>
