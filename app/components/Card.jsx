8b184f6e8f3a0c77eb7ab5d461429b96e3c65538
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-07-12 20:05:46 +0530

    add color arr

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -3,31 +3,18 @@ import React from 'react'
 function Card({ time, description, topic }) {
 
       
-  
-const colors = [
-  "bg-red-500",
-  "bg-green-500",
-  "bg-blue-500",
-  "bg-yellow-500",
-  "bg-pink-500",
-  "bg-purple-500",
-  "bg-indigo-500",
-  "bg-orange-500",
-  "bg-teal-500",
-  "bg-rose-500",
-  "bg-cyan-500",
-  "bg-emerald-500",
-  "bg-lime-500",
-  "bg-fuchsia-500",
-  "bg-sky-500",
-  "bg-violet-500",
-  "bg-amber-500",
-  "bg-stone-500",
-  "bg-neutral-500",
-  "bg-zinc-500",
-  "bg-gray-500",
-  "bg-slate-500"
-];
+  const colors = [
+    "color-red",
+    "color-blue",
+    "color-green",
+    "color-yellow",
+    "color-purple",
+    "color-pink",
+    "color-orange",
+    "color-teal",
+    "color-indigo",
+  ];
+
 
   const getRandomColor = () => {
     const index = Math.floor(Math.random() * colors.length);
