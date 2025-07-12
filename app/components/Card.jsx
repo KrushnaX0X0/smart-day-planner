3534f7ce4bb11f5bd6dc37bde8947babfa0f6efe
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-07-12 19:28:14 +0530

    add property in css

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({time,description,topic}) {
   return (
-    <div className='h-[50vh] w-56 size-18  z-20 relative  shadow-black shadow-xl/30 bg-white/10 shadow-lg  backdrop-blur-md   rounded-2xl flex flex-col  items-center gap-10 p-5 text-center overflow-hidden '>
+    <div className='h-[52vh] w-60 size-18  z-20 relative  shadow-black shadow-xl/30 bg-white/10 shadow-lg  backdrop-blur-md   rounded-2xl flex flex-col  items-center gap-10 p-5 text-center overflow-hidden '>
           <h1 className='text-2xl z-10'>{topic}</h1>
           <p className='z-10'>{description}</p>
           <span className='z-10'>{time}</span>
